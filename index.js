aae4055017db0e83c0b22001c6f720ed075dfc3b
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-07-08 23:32:17 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("parcelDB");
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("🚀 Pinged your deployment.");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+// Sample route
+app.get("/", (req, res) => {
+  res.send("Parcel Server is running");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
